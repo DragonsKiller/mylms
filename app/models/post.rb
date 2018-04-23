b7ba382f755048c994b1404b3b685f56958cd2bb
DragonsKiller <dragon.airos@yandex.by>
2018-04-23 20:28:53 +0300

    add CRUD and categories

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :category
-  validate: :titl, :content, :category_id, presense: true
+  validates_presence_of :title, :content, :category_id
+  has_many :line_items, inverse_of: :order
 end
